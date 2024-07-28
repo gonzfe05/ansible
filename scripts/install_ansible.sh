295a968bd41db8fa981d81e043134e52788199e9
fernando <fernandogonzalez512@hotmail.com>
2024-07-28 14:08:51 -0300

    add dont ask to apt

--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-apt install software-properties-common
+apt update
+apt install software-properties-common -y
 apt-add-repository --yes --update ppa:ansible/ansible
-apt install ansible
+apt install ansible -y
 #pip3 install ansible-dev-tools
